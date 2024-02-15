59880a7135d673ba96b026a874b695aac40c947a
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-02-15 15:37:48 +0530

    added

---
+++ 8. JavaScript Intro/script.js
@@ -0,0 +1,6 @@
+var firstName = prompt("Enter your first name: ");
+var lastName = prompt("Enter your last name: ");
+var age = prompt("Enter your age: ");
+
+console.log(firstName + " " + lastName);
+console.log(age);
