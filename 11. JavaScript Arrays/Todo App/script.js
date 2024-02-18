0137b387a8b069d57130c3d80785cf3fdfd13267
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-02-18 18:11:16 +0530

    added

--- 11. JavaScript Arrays/Todo App/script.js
+++ 11. JavaScript Arrays/Todo App/script.js
@@ -7,10 +7,18 @@ while (msg != "quit") {
     if (msg == "list")
         console.log(list);
 
-    else {
+    else if (msg == "new") {
 
         var newTodo = prompt("Enter a new todo");
         list.push(newTodo);
+        console.log(newTodo + " added to the list.");
+    }
+
+    else if (msg == "delete") {
+
+        var idx = prompt("Which index would you like to delete?");
+        list.splice(idx, 1);
+        console.log("Todo removed");
     }
 
     msg = prompt("What would you like to do?");
