26be10790c68fa104d3bcc0f3e6e048e993b22d3
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-02-18 18:24:56 +0530

    added

---
+++ 12. JavaScript Objects/intro.js
@@ -0,0 +1,11 @@
+var person = {
+    name:"Colt",
+    age:33,
+    city:"New York"
+}
+
+console.log(person.name);
+
+person["age"]+=1;
+person.city = "Los Angeles";
+console.log(person);
---
+++ 12. JavaScript Objects/quiz.js
@@ -0,0 +1,7 @@
+var someObject = {
+    friends: [{ name: "Malfoy" }, { name: "Crabble" }, { name: "Google" }],
+    color: "baby blue",
+    isEvil: true
+}
+
+console.log(someObject["friends"][0]["name"]);
