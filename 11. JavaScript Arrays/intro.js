f5b920f1b78ff3adf96569f0327593a381e8ad39
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-02-18 17:55:25 +0530

    added

---
+++ 11. JavaScript Arrays/intro.js
@@ -0,0 +1,5 @@
+var arr = [];
+var arr2 = new Array();
+
+arr = [49,true,"Hermoine",null];
+console.log(arr.length);
---
+++ 11. JavaScript Arrays/methods.js
@@ -0,0 +1,18 @@
+var arr = [1, 2, 3, 4];
+
+arr.push(5);
+console.log(arr);
+
+arr.unshift(0);
+console.log(arr);
+
+arr.pop();
+console.log(arr);
+
+arr.shift();
+console.log(arr);
+
+console.log(arr.indexOf(14));
+
+var arr2 = arr.slice(0);
+console.log(arr2);
