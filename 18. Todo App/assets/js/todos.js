2aec9284621fca41de0f15de37cefc621bb023fa
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-02-20 12:46:50 +0530

    added

---
+++ 18. Todo App/assets/js/todos.js
@@ -0,0 +1,22 @@
+$('ul').on("click", "li", function () {
+    $(this).toggleClass('completed');
+})
+
+$('ul').on("click", "span", function (event) {
+
+    $(this).parent().fadeOut(500, function () {
+        $(this).remove();
+    })
+
+    event.stopPropagation();
+})
+
+$('input').keypress(function (event) {
+
+    if (event.which === 13) {
+
+        var todo = $(this).val();
+        $(this).val("");
+        $('ul').append("<li><span>X</span> " + todo + "</li>");
+    }
+})
