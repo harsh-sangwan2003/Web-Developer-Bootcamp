7765f05f99335e8e443de123bbc398a99fecc453
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-03-11 12:53:12 +0530

    added

--- 22. Intermediate Express/ejs-demo/app.js
+++ 22. Intermediate Express/ejs-demo/app.js
@@ -5,6 +5,23 @@ app.get("/", (req, res) => {
     res.render('home.ejs');
 })
 
+app.get('/fallinlovewith/:thing', (req, res) => {
+
+    const name = req.params.thing;
+    res.render('love.ejs', { name });
+})
+
+app.get('/posts', (req, res) => {
+
+    const posts = [
+        { title: "Post 1", author: "Susy" },
+        { title: "Post 2", author: "Charlie" },
+        { title: "Post 2", author: "Colt" },
+    ]
+
+    res.render('posts.ejs', { posts });
+})
+
 app.listen(3000, () => {
     console.log("Backend server is running!");
 })
